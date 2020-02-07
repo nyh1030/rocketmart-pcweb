249cce485b351113c58480a0b05908523824b12d
nyh1030 <nyhpig@gmail.com>
2020-02-07 18:40:27 +0900

    초기

---
+++ src/main/java/com/rocketmart/pcweb/RocketmartPcwebApplication.java
@@ -0,0 +1,13 @@
+package com.rocketmart.pcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmartPcwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmartPcwebApplication.class, args);
+    }
+
+}
