563f11652ebef5d8d433a9f866f0d101883424ea
nyh1030 <nyhpig@gmail.com>
2020-02-23 12:34:31 +0900

    회원가입 수정

--- src/main/java/com/rocketmart/pcweb/biz/svc/MemberSvc.java
+++ src/main/java/com/rocketmart/pcweb/biz/svc/MemberSvc.java
@@ -1,26 +1,37 @@
 package com.rocketmart.pcweb.biz.svc;
 
-import com.rocketmart.pcweb.biz.dao.MemberDto;
-import com.rocketmart.pcweb.biz.dao.MemberRepository;
+import com.rocketmart.pcweb.biz.dao.dto.MemberDto;
+import com.rocketmart.pcweb.biz.dao.repository.MemberRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
+@AllArgsConstructor
 @Service
 public class MemberSvc {
 
 	@Autowired
-	private MemberRepository memberMapper;
+	private MemberRepository memberRepository;
 
 	@Transactional
-	public void saveOneForMem(MemberDto memberDto) {
+	public Map<String, Object> saveOneForMemInfo(MemberDto memberDto) {
 		// 비밀번호 암호화
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		memberDto.setMemPw(passwordEncoder.encode(memberDto.getMemPw()));
 
-		memberMapper.saveOneForMem(memberDto);
+		memberRepository.saveOneForMemInfo(memberDto);
+
+		int resultCnt = memberRepository.saveOneForMemInfo(memberDto);
+		Map<String, Object> returnMap = new HashMap<>();
+		returnMap.put("resultCode", resultCnt > 0 ? "200" : "-1");
+		returnMap.put("resultMsg", resultCnt > 0 ? "SUCCESS" : "FAIL");
+		return returnMap;		
 	}
 }
