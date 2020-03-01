1439088f10758141fa9dc3d915763a1b7b230ced
gomshot <gomshot7@gmail.com>
2020-03-02 00:15:16 +0900

    - 판매자 정보수정 사업자유형 수정

--- src/main/java/com/rocketmart/pcweb/biz/ctl/web/MemberCtl.java
+++ src/main/java/com/rocketmart/pcweb/biz/ctl/web/MemberCtl.java
@@ -10,6 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class MemberCtl {
--- src/main/java/com/rocketmart/pcweb/biz/svc/MemberSvc.java
+++ src/main/java/com/rocketmart/pcweb/biz/svc/MemberSvc.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -58,11 +59,13 @@ public class MemberSvc {
 	 */
 	public Map<String, Object> findOneForMemInfo(String mmbrId) {
 
-
+		List<Map<String, Object>> bsnsList = null;	// List
 		Map<String, Object> mmbrMap = null;		// 회원정보 Map
+		Map<String, Object> bsnsTypMap = null;	// 사업자유형 Map
 		Object bsnsTyp = null;				// 사업자유형
-		String[] bsnsTypCdArr = null;		// 사업자유형 코드 Array
-		String[] bsnsTypNmArr = null;		//  사업자유형 코드명 Array
+		String[] bsnsTypCds = null;		// 사업자유형 코드 Array
+		String[] bsnsTypCdNms = null;	// 사업자유형 코드명 Array
+		String[] bsnsChckArr = null;	// 사업자유형 선택 Array
 		String role = "";	// 역할
 		int indx = 0;
 
@@ -70,35 +73,49 @@ public class MemberSvc {
 		mmbrMap = this.memberRepository.findOneForMemInfo(mmbrId);
 		role = String.valueOf(mmbrMap.get("ROLE"));
 
-		if(role.equals("admin")) {	// 관리자
+		if(role.equals("ADMIN")) {	// 관리자
 
-		}else if(role.equals("buyer")) {	// 구매자
+		}else if(role.equals("BUYER")) {	// 구매자
 
-		}else if(role.equals("seller")) {	// 판매자
+		}else if(role.equals("SELLER")) {	// 판매자
 
+			bsnsTypCds = new String[3];
+			bsnsTypCds[0] = "10";
+			bsnsTypCds[1] = "20";
+			bsnsTypCds[2] = "30";
+			bsnsTypCdNms = new String[3];
+			bsnsTypCdNms[0] = "제조업자";
+			bsnsTypCdNms[1] = "책임판매업자";
+			bsnsTypCdNms[2] = "도매/소매 유통업자";
+
+			bsnsList = new ArrayList<Map<String, Object>>();
 			bsnsTyp = mmbrMap.get("BSNS_TYPE");	// 사업자유형
 
 			if(bsnsTyp != null) {
+				bsnsChckArr = String.valueOf(bsnsTyp).split(",");
+			}
 
-				bsnsTypCdArr = String.valueOf(bsnsTyp).split(",");
-				bsnsTypNmArr = new String[bsnsTypCdArr.length];
+			for(int i=0; i<bsnsTypCds.length; i++) {
 
-				for(String typCd : bsnsTypCdArr) {
+				bsnsTypMap = new HashMap<String, Object>();
+				bsnsTypMap.put("bsnsCd", bsnsTypCds[i]);
+				bsnsTypMap.put("bsnsCdNm", bsnsTypCdNms[i]);
+				bsnsTypMap.put("chckFlg", false);
 
-					if (typCd.equals("10")) {
-						bsnsTypNmArr[indx] = "- 제조업";
-					} else if (typCd.equals("20")) {
-						bsnsTypNmArr[indx] = "- 책임판매업";
-					} else if (typCd.equals("30")) {
-						bsnsTypNmArr[indx] = "- 도매/소매 유통업자";
+				for(int j=0; j<bsnsChckArr.length; j++) {
+
+					if(bsnsTypCds[i].equals(bsnsChckArr[j])) {
+						bsnsTypMap.put("chckFlg", true);
+						break;
 					}
-
-					indx++;
 				}
 
-				mmbrMap.put("bsnsTyps", bsnsTypNmArr);	// 사업자유형 저장
+				bsnsList.add(bsnsTypMap);
 			}
 		}
+
+		mmbrMap.put("bsnsList", bsnsList);
+
 
 		return mmbrMap;
 	}
