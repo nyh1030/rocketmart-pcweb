638925d4970a2a978313a435b6064f5c051795a5
nyh1030 <nyhpig@gmail.com>
2020-03-06 16:27:34 +0900

    카테고리 목록조회

--- src/main/java/com/rocketmart/pcweb/biz/dao/repository/CategoryRepository.java
+++ src/main/java/com/rocketmart/pcweb/biz/dao/repository/CategoryRepository.java
@@ -8,7 +8,12 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Map;
 
+import static com.rocketmart.jooq.Tables.TB_BRAND_MST;
 import static com.rocketmart.jooq.Tables.TB_CATE_MST;
+import static com.rocketmart.jooq.tables.TbCmAfile.TB_CM_AFILE;
+import static com.rocketmart.jooq.tables.TbMemMst.TB_MEM_MST;
+import static com.rocketmart.jooq.tables.TbPrdMst.TB_PRD_MST;
+import static com.rocketmart.pcweb.common.CommonUtils.isNotEmpty;
 
 @Repository
 public class CategoryRepository {
@@ -26,4 +31,32 @@ public class CategoryRepository {
                 .orderBy(TB_CATE_MST.CATE1_CD.asc(), TB_CATE_MST.CATE2_CD.asc(), TB_CATE_MST.CATE3_CD.asc())
                 .fetchMaps();
     }
+
+    /**
+     * 메인 > Category 목록 조회
+     * @return List<Map<String, Object>>
+     */
+    public List<Map<String, Object>> findAllForCategoryInfo(TbCateMstRecord tbCateMstRecord) {
+
+        return this.dslContext
+                .select(
+                         TB_PRD_MST.PRODUCT_SEQ
+                        ,TB_PRD_MST.PRODUCT_NM
+                        ,TB_BRAND_MST.BRAND_NM
+                        ,TB_CM_AFILE.AFILE_SEQ
+                        ,TB_CM_AFILE.URL_PATH_CD
+                )
+                .from(TB_PRD_MST)
+                .innerJoin(TB_CM_AFILE)
+                .on(TB_PRD_MST.PRODUCT_SEQ.equal(TB_CM_AFILE.AFILE_SEQ))
+                .innerJoin(TB_BRAND_MST)
+                .on(TB_PRD_MST.BRAND_SEQ.equal(TB_BRAND_MST.BRAND_SEQ))
+                .where(TB_PRD_MST.DEL_YN.equal("N"))
+                .and(isNotEmpty(tbCateMstRecord.getCate1Cd(), TB_PRD_MST.CATE1_CD.eq(tbCateMstRecord.getCate1Cd())))
+                .and(isNotEmpty(tbCateMstRecord.getCate2Cd(), TB_PRD_MST.CATE2_CD.eq(tbCateMstRecord.getCate2Cd())))
+                .and(isNotEmpty(tbCateMstRecord.getCate3Cd(), TB_PRD_MST.CATE3_CD.eq(tbCateMstRecord.getCate3Cd())))
+                .orderBy(TB_PRD_MST.REG_TS.desc())
+                .fetchMaps();
+    }
+
 }
