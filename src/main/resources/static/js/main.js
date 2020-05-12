19a48f7998eec0c16dddfec050930c9bf10b1cd2
미래 <loulouwon@gmail.com>
2020-05-12 22:55:16 +0900

    메인 슬라이드 기능 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -374,15 +374,15 @@
             $(this).closest('li').siblings('li').removeClass('active');
             $(this).closest('li').addClass('active');
             $(target).addClass('active');
-            $(target).siblings('.ps-tab').removeClass('active');
+            $(target).siblings('div').removeClass('active');
         });
         $('.ps-tab-list.owl-slider .owl-item a').on('click', function(e) {
             e.preventDefault();
             var target = $(this).attr('href');
             $(this).closest('.owl-item').siblings('.owl-item').removeClass('active');
             $(this).closest('.owl-item').addClass('active');
-            $(target).addClass('active');
-            $(target).siblings('.ps-tab').removeClass('active');
+            $(target).addClass('active').show();
+            $(target).siblings('div').removeClass('active').hide();
         });
     }
 
